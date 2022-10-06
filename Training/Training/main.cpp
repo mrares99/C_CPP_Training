2120fdc0f298121a7054a01c863ba62555182dbd
Rares Moldovan <rares.moldo997@gmail.com>
2022-10-06 14:37:06 +0300

    Explicit keyword used to prove that conversion takes place

--- Training/Training/main.cpp
+++ Training/Training/main.cpp
@@ -5,26 +5,27 @@
 #include "Person.h"
 #include "Person.cpp" //I know it is wrong, but without doing this, I have a linker issue
 
+class Test {
+private:
+	int value;
+public:
+	explicit Test(int value) {
+		this->value = value;
+	}
+	
+	int getValue() {
+		return value;
+	}
+};
+
+void func(Test test) {
+	int i = test.getValue();
+}
+
 int main()
 {
-	std::unique_ptr<Person<int>> person(nullptr);
-	person = std::make_unique<Person<int>>(new Person<int>{});
-	person->printFields();
-	person.reset(); //free the memory
-
-	std::unique_ptr<Person<int>> person2 = std::make_unique<Person<int>>(new Person<int>{});
-	person2->printFields();
-
-	std::shared_ptr<Person<int>> person3(nullptr);
-	person3 = std::make_shared<Person<int>>(new Person<int>{});
-	person3->printFields();
-
-	std::shared_ptr<Person<int>> person4(nullptr);
-	person4 = person3; // Increment counter references
-	std::shared_ptr<Person<int>> person5(nullptr);
-	person5 = person4;
 	
-	std::weak_ptr<Person<int>> person5(person3);
+	func(2);
 
 	return 0;
 }
